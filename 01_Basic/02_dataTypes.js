12933f5847a258c3495a045079a672da0960a758
roguecodes <you@example.com>
2023-11-09 10:46:54 +0530

    DataTypes & ConversionProcess

--- 01_Basic/02_dataTypes.js
+++ 01_Basic/02_dataTypes.js
@@ -1,6 +1,26 @@
+//Primitive DataTypes 
+
 // number => 2
 // bigint
 //string => "asd34"
 // boolean => true/false
 // null => standalone value 
 // undefined => unassigned variable
+//symbol => unique representation
+
+//Non Primitive Datatypes
+
+/*object =>
+ {
+key:"value"
+}
+*/
+
+/*
+The Primitive Data types in JavaScript include Number, String, Boolean, Undefined, Null and Symbol.
+
+The Non-Primitive data type has only one member i.e. the Object.
+*/
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+console.log(Number(score))
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = 1
+
+// true => 1; false => 0
+//""=> false
+//"hitesh" => true
+
+console.log(Boolean(1));
+console.log(false && 1)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
